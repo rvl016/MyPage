cfa7ca802772b4b1973cd9089a978df965853fb8
Ravi do Valle Luz <ravi.luz@usp.br>
2020-05-09 21:49:58 -0300

    More pages.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# AngularConwaysGame
+# myPage
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.1.0.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angularConwaysGame'`, () => {
+  it(`should have as title 'myPage'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angularConwaysGame');
+    expect(app.title).toEqual('myPage');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angularConwaysGame app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('myPage app is running!');
   });
 });
